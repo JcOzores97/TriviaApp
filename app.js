44cda960335afb53b498c5ee69467baa0744e007
Juan Cruz Ozores <juanozores97@gmail.com>
2021-01-08 17:59:53 -0300

    fix: api middleware problem

--- app.js
+++ app.js
@@ -10,11 +10,11 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 // app.use(express.static(path.join(__dirname, 'public')));
 if (process.env.NODE_ENV === 'production') {
+	app.use('/api', apiRouter);
 	app.use(express.static(path.join(__dirname, 'client/build')));
 	app.get('/*', (req, res) => {
 		res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
 	});
 }
-app.use('/api', apiRouter);
 
 module.exports = app;
