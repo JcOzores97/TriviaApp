f2ca156f573cac1d9a709c37d601e80db0331b16
Juan Cruz Ozores <juanozores97@gmail.com>
2021-01-14 21:30:39 -0300

    fix: package.json (client and server) scripts and proxy

--- app.js
+++ app.js
@@ -9,12 +9,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 // app.use(express.static(path.join(__dirname, 'public')));
+app.use('/api', apiRouter);
 if (process.env.NODE_ENV === 'production') {
-	app.use('/api', apiRouter);
-	app.use(express.static(path.join(__dirname, 'client/build')));
-	app.get('/*', (req, res) => {
-		res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
-	});
+	app.use(express.static('client/build'));
 }
-
 module.exports = app;
