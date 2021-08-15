332b3c6b979a73959b192f17cf5342e07bf2f79f
Juan Cruz Ozores <juanozores97@gmail.com>
2021-08-15 20:57:55 -0300

    add production configuration

--- client/src/contexts/socket.js
+++ client/src/contexts/socket.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import io from 'socket.io-client';
 
-const socket = io(`http://localhost:8080`); //development server runs in 8080, client in 3000
+const socket = io(); //development server runs in 8080, client in 3000
 
 const SocketContext = React.createContext(socket);
 
