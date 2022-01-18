06a037501d4a07e17b697faca65fc2013c2d0c5f
Juan Cruz Ozores <juanozores97@gmail.com>
2022-01-17 22:10:12 -0300

    add final results confetti

--- client/src/components/finalResults/FinalResults.js
+++ client/src/components/finalResults/FinalResults.js
@@ -1,9 +1,59 @@
-import React from 'react';
+import React, { useState, useRef, useEffect } from 'react';
+import ReactCanvasConfetti from 'react-canvas-confetti';
 import './FinalResults.css';
 import { ReactComponent as StarIcon } from '../icons/starIcon.svg';
 const FinalResults = ({ gameScore }) => {
+	const refAnimationInstance = useRef(null);
+
+	const getInstance = (instance) => {
+		refAnimationInstance.current = instance;
+	};
+
+	const makeShot = (particleRatio, opts) => {
+		refAnimationInstance.current &&
+			refAnimationInstance.current({
+				...opts,
+				origin: { y: 0.4 },
+				particleCount: Math.floor(200 * particleRatio)
+			});
+	};
+
+	const fire = () => {
+		makeShot(0.25, {
+			spread: 26,
+			startVelocity: 55
+		});
+
+		makeShot(0.2, {
+			spread: 60
+		});
+
+		makeShot(0.35, {
+			spread: 100,
+			decay: 0.91,
+			scalar: 0.8
+		});
+
+		makeShot(0.1, {
+			spread: 120,
+			startVelocity: 25,
+			decay: 0.92,
+			scalar: 1.2
+		});
+
+		makeShot(0.1, {
+			spread: 120,
+			startVelocity: 45
+		});
+	};
+
+	useEffect(() => {
+		fire();
+	}, []);
+
 	return (
 		<div className="final-results">
+			<ReactCanvasConfetti className={'final-results__confetti'} refConfetti={getInstance} />
 			<div className="final-results__star-container">
 				<StarIcon />
 			</div>
